0f6049a68b2c6715f92e39fcdf8c8673b2239b9a
Ron George Valiyaveettil <ron2george@gmail.com>
2020-07-12 01:06:32 +0530

    #2

--- ocr.py
+++ ocr.py
@@ -2,30 +2,29 @@
 from PIL import Image 
 import pytesseract 
 import sys 
-#from pdf2image import convert_from_path 
 import os 
   
 ''' 
 Part #2 - Recognizing text from the images using OCR 
 '''
 
-def OCR(filename)  
-# Creating a text file to write the output 
-outfile = os.path.splitext(filename,[0])
+def OCR(filename):  
+    # Creating a text file to write the output 
+    outfile = os.path.splitext(filename,[0])
   
-# Open the file in write mode so that  
-# previous texts in file will be erased 
-f = open(outfile, "w") 
+    # Open the file in write mode so that  
+    # previous texts in file will be erased 
+    f = open(outfile, "w") 
   
-image = Image.open(filename)
-text =''
-for frame in range (image.n_frames):
-	image.seek(frame)
+    image = Image.open(filename)
+    text =''
+    for frame in range (image.n_frames):
+	    image.seek(frame)
           
     	# Recognize the text as string in image using pytesserct 
-	try:
-		text += str(((pytesseract.image_to_string(image,lang="Malayalam"))))
-	except RuntimeError as timeout_error:
+	    try:
+		    text += str(((pytesseract.image_to_string(image,lang="Malayalam"))))
+	    except RuntimeError as timeout_error:
     		print("terminated")	
     		# Tesseract processing is terminated
     		pass 
@@ -39,11 +38,11 @@ for frame in range (image.n_frames):
     # Eg: This is a sample text this word here Mala- 
     # yalam is half on first line, remaining on next. 
     # To remove this, we replace every '-\n' to ''. 
-text = text.replace('-\n', '')     
+    text = text.replace('-\n', '')     
   
-# Finally, write the processed text to the file. 
-f.write(text) 
+    # Finally, write the processed text to the file. 
+    f.write(text) 
   
-# Close the file after writing all the text. 
-f.close() 
+    # Close the file after writing all the text. 
+    f.close() 
 
